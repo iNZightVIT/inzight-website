5cbb7492d38a1fe18aa83813d0c480fb32a69f9c
Tom Elliott <tomelliottnz@gmail.com>
2022-08-23 16:06:21 +1200

    adding colour

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,10 +42,11 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/iNZightVIT/inzight-website/edit/main/",
+          // "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           remarkPlugins: [oembed, [smartypants, { dashes: "oldschool" }], math],
           rehypePlugins: [katex],
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
