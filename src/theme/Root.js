bee7ea0ba2553683f93af4c07fac4cac0a27383e
Tom Elliott <tom.elliott@auckland.ac.nz>
2023-06-23 15:46:29 +1200

    only use localStorage in browser

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -4,15 +4,20 @@ const VersionContext = React.createContext();
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
-  let v = localStorage.getItem("version");
-  if (!v && v !== "desktop" && v !== "lite") {
-    localStorage.setItem("version", "desktop");
-    v = "desktop";
+  if (typeof window !== 'undefined') {
+    let v = localStorage.getItem("version");
+    if (!v && v !== "desktop" && v !== "lite") {
+      localStorage.setItem("version", "desktop");
+      v = "desktop";
+    }
+  } else {
+    let v = "desktop";
   }
   const [version, setVersion] = React.useState(v);
 
   useEffect(() => {
-    localStorage.setItem("version", version);
+    if (typeof window !== 'undefined')
+      localStorage.setItem("version", version);
   }, [version]);
 
   return (
