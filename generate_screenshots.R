3126d704f0be74c065ad3565be299e601bf431f9
Tom Elliott <tom.elliott@auckland.ac.nz>
2023-06-23 15:39:38 +1200

    Add screenshot script

---
+++ generate_screenshots.R
@@ -0,0 +1,35 @@
+# Run using the R in the iNZight folder, otherwise the graphics don't
+# work correctly.
+
+inzight_dir <- file.path("~", "iNZightVIT")
+if (!is.null(inzight_dir) && dir.exists(inzight_dir)) {
+    .libPaths(file.path(inzight_dir, "library"), include.site = FALSE)
+} else if (dir.exists(file.path("~", "iNZightVIT-dev", "library"))) {
+    .libPaths(file.path("~", "iNZightVIT-dev", "library"),
+        include.site = FALSE)
+} else {
+    .libPaths(file.path("~", "iNZightVIT", "library"),
+        include.site = FALSE)
+}
+
+if (!requireNamespace("screenshot", quietly = TRUE))
+    remotes::install_github("iNZightVIT/screenshot")
+library(screenshot)
+library(iNZight)
+ui <- iNZight()
+old_prefs <- ui$preferences
+
+ui$preferences$popout <- FALSE
+ui$preferences$dev.features <- FALSE
+ui$preferences$window.size <- c(1040, 640)
+ui$savePreferences()
+ui$reload()
+
+ui$close()
+
+options("screenshot.add.grid" = TRUE) # set to FALSE when finished
+
+crawl("docs")
+
+ui$preferences <- old_prefs
+ui$savePreferences()
